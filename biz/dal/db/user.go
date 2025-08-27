1e157a4123a5f3d2505cb03682b462ca551098ea
ybs <2112536022@qq.com>
2025-08-27 17:22:25 +0800

    fix:add user mysql

--- biz/dal/db/user.go
+++ biz/dal/db/user.go
@@ -1,1 +1,71 @@
 package db
+
+import (
+	"tiktok/pkg/constants"
+	"tiktok/pkg/errno"
+)
+
+type User struct {
+	ID              int64  `json:"id"`
+	UserName        string `json:"user_name"`
+	Password        string `json:"password"`
+	Avatar          string `json:"avatar"`
+	BackgroundImage string `json:"background_image"`
+	Signature       string `json:"signature"`
+}
+
+func (User) TableName() string {
+	return constants.UserTableName
+}
+
+func CreateUser(user *User) (int64, error) {
+	err := DB.Create(user).Error
+	if err != nil {
+		return 0, err
+	}
+	return user.ID, err
+}
+
+func QueryUser(username string) (*User, error) {
+	var user User
+	err := DB.Where("user_name = ?", username).Find(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func QueryUserById(user_id int64) (*User, error) {
+	var user User
+	if err := DB.Where("id = ?", user_id).Find(&user).Error; err != nil {
+		return nil, err
+	}
+	if user == (User{}) {
+		err := errno.UserIsNotExistErr
+		return nil, err
+	}
+	return &user, nil
+}
+
+func VerifyUser(userName, password string) (int64, error) {
+	var user User
+	if err := DB.Where("user_name = ? AND password = ?", userName, password).Find(&user).Error; err != nil {
+		return 0, err
+	}
+	if user.ID == 0 {
+		err := errno.PasswordIsNotVerified
+		return user.ID, err
+	}
+	return user.ID, nil
+}
+
+func CheckUserExistById(user_id int64) (bool, error) {
+	var user User
+	if err := DB.Where("id = ?", user_id).Find(&user).Error; err != nil {
+		return false, err
+	}
+	if user == (User{}) {
+		return false, nil
+	}
+	return true, nil
+}
